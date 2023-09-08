1be6f661cac58dd427f2c3fd1d2fe9cbf747c17a
61610@etu.he2b.be <61610@etu.he2b.be>
2023-09-08 18:55:05 +0200

    mmm

--- Script/navigation.js
+++ Script/navigation.js
@@ -9,30 +9,30 @@ if (lightmode == "true"){
 function enablelightmode(){
     document.body.classList.add("light-theme");
     localStorage.setItem("light-mode", "true");
+    document.getElementById("night-light-mode").style.backgroundColor = "white";
+    document.getElementById("night-light-mode").style.borderColor = "black";
+    document.getElementById("fa-moon").style.color = "black";
+    document.getElementById("fa-moon").classList.remove("fa-sun");
+    document.getElementById("fa-moon").classList.add("fa-moon");
+    document.getElementById("imgbutton").src = "./img/discord-button-lighttheme.png"
 }
 
 function disablelightmode(){
     document.body.classList.remove("light-theme");
     localStorage.setItem("light-mode", "false");
+    document.getElementById("night-light-mode").style.backgroundColor = "black";
+    document.getElementById("night-light-mode").style.borderColor = "white";
+    document.getElementById("fa-moon").style.color = "white";
+    document.getElementById("fa-moon").classList.remove("fa-moon");
+    document.getElementById("fa-moon").classList.add("fa-sun");
+    document.getElementById("imgbutton").setAttribute("src", "/img/discord-button.png")
 }
 
 document.getElementById("night-light-mode").addEventListener("click", ()=>{
     if (localStorage.getItem("light-mode") == "false") {
         enablelightmode();
-        document.getElementById("night-light-mode").style.backgroundColor = "white";
-        document.getElementById("night-light-mode").style.borderColor = "black";
-        document.getElementById("fa-moon").style.color = "black";
-        document.getElementById("fa-moon").classList.remove("fa-sun");
-        document.getElementById("fa-moon").classList.add("fa-moon");
-        document.getElementById("imgbutton").src = "./img/discord-button-lighttheme.png"
     } else {
         disablelightmode();
-        document.getElementById("night-light-mode").style.backgroundColor = "black";
-        document.getElementById("night-light-mode").style.borderColor = "white";
-        document.getElementById("fa-moon").style.color = "white";
-        document.getElementById("fa-moon").classList.remove("fa-moon");
-        document.getElementById("fa-moon").classList.add("fa-sun");
-        document.getElementById("imgbutton").setAttribute("src", "/img/discord-button.png")
     }
 })
 
