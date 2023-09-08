7bc2b040192e34553ee59658fbe106c0ea58ca38
61610@etu.he2b.be <61610@etu.he2b.be>
2023-09-08 14:57:07 +0200

    updated

---
+++ Script/navigation.js
@@ -0,0 +1,30 @@
+let lightmode = false;
+
+document.getElementById("joindiscordbutton").addEventListener("click", ()=>{
+    window.open("https://discord.gg/CUbwjYHt");
+})
+
+document.getElementById("night-light-mode").addEventListener("click", ()=>{
+    if (lightmode) {
+        lightmode = false;
+    } else {
+        lightmode = true;
+    }
+    document.body.classList.toggle("light-theme");
+    if (lightmode) {
+        document.getElementById("night-light-mode").style.backgroundColor = "white";
+        document.getElementById("night-light-mode").style.borderColor = "black";
+        document.getElementById("fa-moon").style.color = "black";
+        document.getElementById("fa-moon").classList.remove("fa-sun");
+        document.getElementById("fa-moon").classList.add("fa-moon");
+        document.getElementById("imgbutton").src = "./img/discord-button-lighttheme.png"
+    } else {
+        document.getElementById("night-light-mode").style.backgroundColor = "black";
+        document.getElementById("night-light-mode").style.borderColor = "white";
+        document.getElementById("fa-moon").style.color = "white";
+        document.getElementById("fa-moon").classList.remove("fa-moon");
+        document.getElementById("fa-moon").classList.add("fa-sun");
+        document.getElementById("imgbutton").setAttribute("src", "/img/discord-button.png")
+    }
+
+})
